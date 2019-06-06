9d24c618fccdf43587df4f43709b38c6d41c57e6
PinPoint <ms1b.139@gmail.com>
2019-06-06 19:07:24 +0200

    only 1 client possible

--- server/events.py
+++ server/events.py
@@ -8,34 +8,41 @@ socketID = []
 
 #assign a unique ID to a new player
 def initialize_player(id):
-    if (len(clients) == 0):
-        # TODO: stworzyc klase Gra i niech  se gra
-        # k = Klient.Klient(0)
-        k = engine.Gra(0)
-        # k = gra.Gra()
-        clients.append(k)
-        clientIDs.append(0)
-        socketID.append(id)
-        emit('init', 0)
-    else:
-        for i in range(len(clients)):
-            if i not in clientIDs:
-                clientIDs.append(i)
-                socketID.append(id)
-                k = engine.Gra(i)
-                clients.append(k)
-                emit('init', i)
-                break
-            if i == len(clients)-1:
-                i+=1
-                k = engine.Gra(i)
-                clientIDs.append(i)
-                socketID.append(id)
-                clients.append(k)
-                emit('init', i)
-    # print("\n\n\nadding\n")
-    print(socketID)
-    print(clientIDs)
+    k = engine.Gra(0)
+    k.play()
+    k.get
+    print("playing")
+
+    # if (len(clients) == 0):
+    #     # TODO: stworzyc klase Gra i niech  se gra
+    #     # k = Klient.Klient(0)
+    #     k = engine.Gra(0)
+    #     # k = gra.Gra()
+    #     clients.append(k)
+    #     clientIDs.append(0)
+    #     socketID.append(id)
+    #     emit('init', 0)
+    # else:
+    #     for i in range(len(clients)):
+    #         if i not in clientIDs:
+    #             clientIDs.append(i)
+    #             socketID.append(id)
+    #             k = engine.Gra(i)
+    #             clients.append(k)
+    #             emit('init', i)
+    #             break
+    #         if i == len(clients)-1:
+    #             i+=1
+    #             k = engine.Gra(i)
+    #             clientIDs.append(i)
+    #             socketID.append(id)
+    #             clients.append(k)
+    #             emit('init', i)
+    # # print("\n\n\nadding\n")
+    # id = 0
+    # clients[socketID.index(id)].play()
+    # print(socketID)
+    # print(clientIDs)
     # print("\n\n\n\n")
 
 
@@ -54,8 +61,9 @@ def handle_move(data, id):
     int_board = data['board']
     # print(int_board)
     # k = Klient.Klient(5)
-    print(int(clients[socketID.index(id)].player1.getID()))
-    clients[socketID.index(id)].player1.convert_and_update(int_board)
+    clients[socketID.index(id)].play()
+    # print(int(clients[socketID.index(id)].player1.getID()))
+    # clients[socketID.index(id)].player1.convert_and_update(int_board)
     move = {'px': [1, 0], 'py': [2, 3], 'tx': [0, 1], 'ty': [3, 4], 'rx':[], 'ry':[]}
     move['px'] = input("px")
     move['py'] = input("py")
